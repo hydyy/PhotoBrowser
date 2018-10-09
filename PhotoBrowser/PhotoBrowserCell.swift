892257cfa416cd1615edfcce2e23abaf0d51b4cf
libingting <lbti@qq.com>
2018-10-09 23:02:36 +0800

    支持显示动图

--- PhotoBrowser/PhotoBrowserCell.swift
+++ PhotoBrowser/PhotoBrowserCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 public protocol PhotoBrowserCellDelegate: NSObjectProtocol {
     /// 拖动时回调。scale:缩放比率
@@ -58,7 +59,7 @@ open class PhotoBrowserCell: UICollectionViewCell {
     open var photoLoader: PhotoLoader?
 
     /// 显示图像
-    open var imageView = UIImageView()
+    open var imageView = AnimatedImageView()
 
     /// 保存原图url，用于点查看原图时使用
     open var rawUrl: URL?
